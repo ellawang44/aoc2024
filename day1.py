f7288e33dce411d97aa72db84003d2ea0244bbec
Ella Xi Wang <ella.xi.wang@gmail.com>
2024-12-02 14:50:46 +0100

    update name to be more descriptive

--- day1.py
+++ day1.py
@@ -2,14 +2,14 @@ import numpy as np
 
 data = np.loadtxt('input.txt')
 
-# chall 1
-print('chall 1', int(np.sum(np.abs(data[:,0] - data[:,1]))))
+# challenge 1
+print('challenge 1', int(np.sum(np.abs(data[:,0] - data[:,1]))))
 
-# chall 2
+# challenge 2
 count = 0
 unique = np.unique(data[:,1], return_counts=True)
 for num in data[:,0]:
     if num in unique[0]:
         count += num*unique[1][np.where(unique[0] == num)[0][0]]
-print('chall 2', int(count))
+print('challenge 2', int(count))
 
