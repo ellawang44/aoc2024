dbe3fe7c842e01b1852c00455b7ca2926f87a669
Ella Xi Wang <ella.xi.wang@gmail.com>
2024-12-14 02:56:34 +0100

    god I should return my math minor, linear equations duh

--- day13.py
+++ day13.py
@@ -1,30 +1,45 @@
-def cost(A, B, prize, lim=100):
-    costs = []
-    num = min(prize[0]//A[0], prize[1]//A[1])
-    for numA in range(num):
-        X_remaining = prize[0]-numA*A[0]
-        Y_remaining = prize[1]-numA*A[1]
-        if X_remaining % B[0] == 0 and Y_remaining % B[1] == 0:
-            numB = X_remaining // B[0]
-            if numB == Y_remaining // B[1]:
-                if numA <= lim and numB <= lim:
-                    costs.append(numA*3+numB)
-    if len(costs) == 0:
+def cost(xA, yA, xB, yB, x, y, lim=100):
+    numB = (y-x*yA/xA)/(yB-xB*yA/xA)
+    numA = (x-numB*xB)/xA
+    if numA > lim or numB > lim:
+        return 0
+    elif round(numA)*xA+round(numB)*xB != x or round(numA)*yA+round(numB)*yB != y:
         return 0
     else:
-        return min(costs)
+        return numA*3+numB
 
 all_cost = []
 with open('data/input_day13.txt') as f:
     for line in f:
         button_A = line.strip().split(': ')[1].split(', ')
-        A = int(button_A[0].split('+')[1]), int(button_A[1].split('+')[1])
+        xA = int(button_A[0].split('+')[1])
+        yA = int(button_A[1].split('+')[1])
         button_B = f.readline().strip().split(': ')[1].split(', ')
-        B = int(button_B[0].split('+')[1]), int(button_B[1].split('+')[1])
+        xB = int(button_B[0].split('+')[1])
+        yB = int(button_B[1].split('+')[1])
         prize_line = f.readline().strip().split(': ')[1].split(', ')
-        prize = int(prize_line[0].split('=')[1]), int(prize_line[1].split('=')[1])
+        x = int(prize_line[0].split('=')[1])
+        y = int(prize_line[1].split('=')[1])
         f.readline()
 
-        all_cost.append(cost(A, B, prize))
+        all_cost.append(cost(xA, yA, xB, yB, x, y))
 
-print('challenge 1', sum(all_cost))
+print('challenge 1', int(sum(all_cost)))
+
+all_cost = []
+with open('data/input_day13.txt') as f:
+    for line in f:
+        button_A = line.strip().split(': ')[1].split(', ')
+        xA = int(button_A[0].split('+')[1])
+        yA = int(button_A[1].split('+')[1])
+        button_B = f.readline().strip().split(': ')[1].split(', ')
+        xB = int(button_B[0].split('+')[1])
+        yB = int(button_B[1].split('+')[1])
+        prize_line = f.readline().strip().split(': ')[1].split(', ')
+        x = int(prize_line[0].split('=')[1])+10000000000000
+        y = int(prize_line[1].split('=')[1])+10000000000000
+        f.readline()
+
+        all_cost.append(cost(xA, yA, xB, yB, x, y, lim=10000000000000000000))
+
+print('challenge 2', int(sum(all_cost)))
